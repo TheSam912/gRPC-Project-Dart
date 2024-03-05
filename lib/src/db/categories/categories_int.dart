b1f7b209df8b099ef891c0efe2900c83dd7435e0
“sobhan”912 <sobhan912@gmail.com>
2024-03-05 11:55:11 +0100

    added return null

--- lib/src/db/categories/categories_int.dart
+++ lib/src/db/categories/categories_int.dart
@@ -3,13 +3,30 @@ import 'package:dart_grpc_server/src/generated/groceries.pb.dart';
 
 abstract class ICategoriesServices {
   factory ICategoriesServices() => CategoriesServices();
-  
-  Category? getCategoryByName(String name){}
-  Category? getCategoryById(int id){}
-  Category? createCategory(Category category){}
-  Category? editCategory(Category category){}
-  Empty? deleteCategory(Category category){}
-  List<Category>? getCategories(){}
+
+  Category? getCategoryByName(String name) {
+    return null;
+  }
+
+  Category? getCategoryById(int id) {
+    return null;
+  }
+
+  Category? createCategory(Category category) {
+    return null;
+  }
+
+  Category? editCategory(Category category) {
+    return null;
+  }
+
+  Empty? deleteCategory(Category category) {
+    return null;
+  }
+
+  List<Category>? getCategories() {
+    return null;
+  }
 }
 
 final categoriesServices = ICategoriesServices();
