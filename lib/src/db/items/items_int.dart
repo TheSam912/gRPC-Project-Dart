bfb8010ffe641fe21d3398db00d06bab7769a3f6
“sobhan”912 <sobhan912@gmail.com>
2024-03-05 11:56:33 +0100

    added return null

--- lib/src/db/items/items_int.dart
+++ lib/src/db/items/items_int.dart
@@ -3,14 +3,33 @@ import 'package:dart_grpc_server/src/generated/groceries.pb.dart';
 
 abstract class IItemsServices {
   factory IItemsServices() => ItemsServices();
-    Item? getItemByName(String name){}
-    Item? getItemById(int id){}
+  Item? getItemByName(String name) {
+    return null;
+  }
 
-  Item? createItem(Item item){}
-  Item? editItem(Item item){}
-  Empty? deleteItem(Item item){}
-  List<Item>? getItems(){}
-  List<Item>? getItemsByCategory(int categoryId){}
+  Item? getItemById(int id) {
+    return null;
+  }
+
+  Item? createItem(Item item) {
+    return null;
+  }
+
+  Item? editItem(Item item) {
+    return null;
+  }
+
+  Empty? deleteItem(Item item) {
+    return null;
+  }
+
+  List<Item>? getItems() {
+    return null;
+  }
+
+  List<Item>? getItemsByCategory(int categoryId) {
+    return null;
+  }
 }
 
 final itemsServices = IItemsServices();
